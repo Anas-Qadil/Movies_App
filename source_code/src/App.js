5449ee791ffb08a15cdf2790368d547439fb82a1
Anas Qadil <aqadil@e2r9p4.1337.ma>
2022-03-29 04:09:48 +0000

    setup files

---
+++ source_code/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./App.css";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
